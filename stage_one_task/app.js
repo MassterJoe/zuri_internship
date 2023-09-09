6eba7a3c6f75a8918e9b4f63d85f97cb1f97e4e7
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-09-09 17:26:04 -0400

    endpoint

--- stage_one_task/app.js
+++ stage_one_task/app.js
@@ -8,30 +8,26 @@ app.get('/', (req, res) => {
     const slack_name = req.query.slack_name;
     const track = req.query.track;
     const time = new Date().toISOString();
-
+    const file_url = 'https://github.com/MassterJoe/zuri_internship/blob/main/stage_one_task/app.js';
+    const repo_url = 'https://github.com/MassterJoe/zuri_internship';
+    
     const day = moment().format('dddd');
     if (!slack_name || !track) {
         return res.status(400).json({ error: 'Both slack_name and track are required.' });
       }
 
     const data = {
-        message: 'Stage one assignment',
-        data: {
             slack_name: slack_name,
             current_day: day,
             utc_time: time,
             track: track,
-            github_file_url: ,
-            github_repo_url: 'https://github.com/MassterJoe/zuri_internship',
-            status_code: '200'
+            github_file_url: file_url ,
+            github_repo_url: repo_url,
+            status_code: 200
         }
-}
-
-
-    
-    
+res.status(200).json(data);
+ 
 })
-
 app.listen(port, () => {
     console.log('Welcome!');
 });
